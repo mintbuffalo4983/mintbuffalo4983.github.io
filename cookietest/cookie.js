c125876d07f24ac03efb5d5d7de5595719707bd9
Mintbuffalo <mintbuffalo4983@gmail.com>
2026-09-23 17:51:09 -0700

    Fix setCookie call to remove expiration days

--- cookietest/cookie.js
+++ cookietest/cookie.js
@@ -27,7 +27,7 @@ function checkCookie() {
     alert("welcome again " + visit);
   } else {
       if (visit != "" && visit != null) {
-      setCookie("hasvisited", yes, 30);
+      setCookie("hasvisited", yes);
      }
   }
 }
